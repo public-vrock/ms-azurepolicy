b6b2b5bb50f6c8eb73639ae37c72ed01088fa15d
Tobias Geiser <tobias.geiser@vrock.ch>
2019-03-19 22:10:45 +0100

    [change] rewrite deploy.ps1

--- apply-mandatory-tags/deploy.ps1
+++ apply-mandatory-tags/deploy.ps1
@@ -1,25 +1,35 @@
-$policyprefix = "[PREFIX]: "
-$managementgroup = Get-AzManagementGroup -GroupName "MgmtGroupName"
-$policysetguid = [guid]::NewGuid()
-$policysetname = ($policyprefix + "Apply mandatory tags on all resources and resource groups")
-$policysetdescription = "This initiative will set policies which apply mandatory tags on all resources and resource groups"
-$policydefinitions = "./apply_mandatory_tags/azurepolicyset.definitions.json"
-$policysetparameters = "./apply_mandatory_tags/azurepolicyset.parameters.json"
+$parameters = Get-Content -Path "./deploy.parameters.json" | ConvertFrom-Json
+
+$definition = @{
+        Name = ([guid]::NewGuid() -replace "-", "").Substring(0,24);
+        DisplayName = "[$($parameters.Company)]: $($parameters.policyset.Name)";
+        Description = $parameters.policyset.Description;
+        scope = Get-AzManagementGroup | Where-Object {$_.DisplayName -eq $parameters.policyset.scope};
+        PolicySetDefinition = "./azurepolicyset.definitions.json";
+        PolicySetParameters = "./azurepolicyset.parameters.json";
+}
+
+$assignment = @{
+        Name = ([guid]::NewGuid() -replace "-", "").Substring(0,24);
+        DisplayName = "[$($parameters.assignment.Type)]: $($parameters.assignment.Name)"
+        Scope = (Get-AzManagementGroup | Where-Object {$_.DisplayName -eq $parameters.assignment.Scope}).Id;
+
+}
 
 $policyset = New-AzPolicySetDefinition `
-        -Name "PolicySetName" `
-        -DisplayName $policysetname `
-        -Description $policysetdescription `
-        -PolicyDefinition $policydefinitions `
-        -Parameter $policysetparameters `
-        -ManagementGroupName $managementgroup.Name
+        -Name $definition.Name `
+        -DisplayName $definition.DisplayName `
+        -Description $definition.Description `
+        -PolicyDefinition $definition.PolicySetDefinition `
+        -Parameter $definition.PolicySetParameters `
+        -ManagementGroupName $definition.scope.Name
  
 New-AzPolicyAssignment `
         -PolicySetDefinition $policyset `
-        -Name "AssignmentName" `
-        -Scope $managementgroup.Id `
-        -DisplayName $policysetname `
-        -Description $policysetname `
-        -CompanyCodeValue "corp" `
-        -CompanyValue "Corporation" `
-        -CustomerIdValue "0000000000"
+        -Name $assignment.Name `
+        -Scope  $assignment.Scope`
+        -DisplayName $assignment.DisplayName `
+        -Description $parameters.assignment.Description `
+        -CompanyCodeValue $parameters.CompanyCode `
+        -CompanyValue $parameters.Company `
+        -CustomerIdValue $parameters.CustomerId
